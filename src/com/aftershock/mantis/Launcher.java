4e5799b851ff5d3719af427fbce19ea82755707f
Luke-Diamond <dripdrop.tech@gmail.com>
2017-01-01 21:43:33 -0500

    Added Fullscreen Option to Launcher.

--- src/com/aftershock/mantis/Launcher.java
+++ src/com/aftershock/mantis/Launcher.java
@@ -22,8 +22,33 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Launcher {
 
+	/**
+	 * Launches a game.
+	 * 
+	 * @param game
+	 *            The game to launch.
+	 * @param title
+	 *            The title of the game window.
+	 * @param width
+	 *            The width of the game window.
+	 * @param height
+	 *            The height of the game window.
+	 * @param resizable
+	 *            Whether or not the game window is resizable.
+	 * @param vsync
+	 *            Whether or not to enable vertical synchronization on the game
+	 *            window.
+	 * @param fullscreen
+	 *            Whether or not to launch the window in fullscreen mode.
+	 * @param samples
+	 *            The number of antialiasing samples.
+	 * @param iconPrefix
+	 *            The prefix for the window icon.
+	 * @return A new instance of the Mantis class, constructed from the given
+	 *         values.
+	 */
 	public static Mantis launch(Game game, String title, int width, int height, boolean resizable, boolean vsync,
-			int samples, String iconPrefix) {
+			boolean fullscreen, int samples, String iconPrefix) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = width;
 		config.height = height;
@@ -31,6 +56,7 @@ public class Launcher {
 		config.samples = samples;
 		config.title = title;
 		config.vSyncEnabled = vsync;
+		config.fullscreen = fullscreen;
 
 		if (iconPrefix != null) {
 			config.addIcon(iconPrefix + "16.png", FileType.Internal);
